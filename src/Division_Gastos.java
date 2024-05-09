930f9892952d8127953da5d9e9d21eba9a915c50
JJ <jrivcan2406@g.educaand.es>
2024-05-09 17:58:01 +0200

    Comentarios añadidos

--- src/Division_Gastos.java
+++ src/Division_Gastos.java
@@ -1,10 +1,11 @@
 public class Division_Gastos {
-    int idDivision;
-    Gasto gasto;
-    Usuario usuarioPagador;
-    Usuario usuarioDebedor;
-    double montoPagado;
-    double montoDebido;
+    //Atributos de la clase
+    int idDivision; //Identificador de la division
+    Gasto gasto; //Gasto al que pertenece la division
+    Usuario usuarioPagador; //Usuario que pagó el gasto
+    Usuario usuarioDebedor; //Usuario que debe pagar el gasto
+    double montoPagado; //Monto pagado por el usuario pagador
+    double montoDebido; //Monto que debe pagar el usuario debedor
 
     //Constructor
     public Division_Gastos(int idDivision, Gasto gasto, Usuario usuarioPagador, Usuario usuarioDebedor, double montoPagado, double montoDebido) {
@@ -14,55 +15,57 @@ public class Division_Gastos {
         this.usuarioDebedor = usuarioDebedor;
         this.montoPagado = montoPagado;
         this.montoDebido = montoDebido;
-    }
+    } //Fin del constructor
 
     //Getters y Setters
 
     public int getIdDivision() {
         return idDivision;
-    }
+    } //Obtener el id de la division
 
     public void setIdDivision(int idDivision) {
         this.idDivision = idDivision;
-    }
+    } //Cambiar el id de la division
 
     public Gasto getGasto() {
         return gasto;
-    }
+    } //Obtener el gasto
 
     public void setGasto(Gasto gasto) {
         this.gasto = gasto;
-    }
+    } //Cambiar el gasto
 
     public Usuario getUsuarioPagador() {
         return usuarioPagador;
-    }
+    } //Obtener el usuario pagador
 
     public void setUsuarioPagador(Usuario usuarioPagador) {
         this.usuarioPagador = usuarioPagador;
-    }
+    } //Cambiar el usuario pagador
 
     public Usuario getUsuarioDebedor() {
         return usuarioDebedor;
-    }
+    } //Obtener el usuario debedor
 
     public void setUsuarioDebedor(Usuario usuarioDebedor) {
         this.usuarioDebedor = usuarioDebedor;
-    }
+    } //Cambiar el usuario debedor
 
     public double getMontoPagado() {
         return montoPagado;
-    }
+    } //Obtener el monto pagado
 
     public void setMontoPagado(double montoPagado) {
         this.montoPagado = montoPagado;
-    }
+    } //Cambiar el monto pagado
 
     public double getMontoDebido() {
         return montoDebido;
-    }
+    } //Obtener el monto debido
 
     public void setMontoDebido(double montoDebido) {
         this.montoDebido = montoDebido;
-    }
+    } //Cambiar el monto debido
+
+
 }
